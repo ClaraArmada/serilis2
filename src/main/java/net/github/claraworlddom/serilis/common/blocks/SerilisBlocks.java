2c1c0d7de83669c9fc289c4d232f5d556878cd50
ClaraArmada <galacticsky.wow@gmail.com>
2024-06-01 10:33:07 +0200

    Initial commit

--- src/main/java/net/github/claraworlddom/serilis/common/blocks/SerilisBlocks.java
+++ src/main/java/net/github/claraworlddom/serilis/common/blocks/SerilisBlocks.java
@@ -1,8 +1,16 @@
 package net.github.claraworlddom.serilis.common.blocks;
 
 import net.github.claraworlddom.serilis.Serilis;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
 
 public class SerilisBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Serilis.MODID);
+
+    public static final Supplier<Block> ROCK = BLOCKS.register("rock",
+            () -> new RockModel(BlockBehaviour.Properties.of().sound(SoundType.STONE).dynamicShape().noOcclusion()));
 }
--- src/main/java/net/github/claraworlddom/serilis/common/items/SerilisItems.java
+++ src/main/java/net/github/claraworlddom/serilis/common/items/SerilisItems.java
@@ -1,15 +1,18 @@
 package net.github.claraworlddom.serilis.common.items;
 
 import net.github.claraworlddom.serilis.Serilis;
+import net.github.claraworlddom.serilis.common.blocks.SerilisBlocks;
 import net.minecraft.world.item.*;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
 
 public class SerilisItems {
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(
-            Serilis.MODID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Serilis.MODID);
 
     public static final Supplier<Item> SHARP_BONE = ITEMS.register("sharp_bone", () ->
             new SwordItem(ItemTiers.BONE, new Item.Properties().stacksTo(1)));
+
+    public static final Supplier<BlockItem> ROCK = ITEMS.registerSimpleBlockItem("rock", SerilisBlocks.ROCK);
+
 }
