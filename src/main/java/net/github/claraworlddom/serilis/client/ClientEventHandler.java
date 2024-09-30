94ceca87650aa9432e9f891fd0c8d96fb6df8494
Clara <galacticsky.wow@gmail.com>
2024-09-30 18:11:54 +0200

    Initial commit

--- src/main/java/net/github/claraworlddom/serilis/client/ClientEventHandler.java
+++
@@ -1,8 +0,0 @@
-package net.github.claraworlddom.serilis.client;
-
-import net.github.claraworlddom.serilis.common.items.SerilisItems;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-
-public class ClientEventHandler {
-    ItemBlockRenderTypes.setRenderLayer(SerilisItems.SWITCHGRASS.get(), RenderType.cutout());
-}
