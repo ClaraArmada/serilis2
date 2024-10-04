5a2a9c21ce1e84c736cda05b99413e48d9037bca
Clara <galacticsky.wow@gmail.com>
2024-10-05 01:41:16 +0200

    Initial commit

--- src/main/java/net/github/claraworlddom/serilis/common/blocks/SerilisBlocks.java
+++ src/main/java/net/github/claraworlddom/serilis/common/blocks/SerilisBlocks.java
@@ -1,10 +1,7 @@
 package net.github.claraworlddom.serilis.common.blocks;
 
 import net.github.claraworlddom.serilis.Serilis;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DoublePlantBlock;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.TallGrassBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
@@ -16,19 +13,26 @@ public class SerilisBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Serilis.MODID);
 
     public static final Supplier<Block> ROCK = BLOCKS.register("rock",
-            () -> new RockModel(BlockBehaviour.Properties.of().sound(SoundType.STONE).dynamicShape().noOcclusion()));
+            () -> new RockModel(BlockBehaviour.Properties.of().sound(SoundType.STONE).dynamicShape().noOcclusion())
+    );
 
     public static final Supplier<Block> FLINT_ROCK = BLOCKS.register("flint_rock",
-            () -> new RockModel(BlockBehaviour.Properties.of().sound(SoundType.STONE).dynamicShape().noOcclusion()));
+            () -> new RockModel(BlockBehaviour.Properties.of().sound(SoundType.STONE).dynamicShape().noOcclusion())
+    );
 
     public static final Supplier<Block> TWIG = BLOCKS.register("twig",
-            () -> new TwigModel(BlockBehaviour.Properties.of().sound(SoundType.WOOD).dynamicShape().noOcclusion()));
+            () -> new TwigModel(BlockBehaviour.Properties.of().sound(SoundType.WOOD).dynamicShape().noOcclusion())
+    );
 
     public static final Supplier<Block> DRIFTWOOD = BLOCKS.register("driftwood",
-            () -> new DriftwoodModel(BlockBehaviour.Properties.of().sound(SoundType.WOOD).dynamicShape().noOcclusion()));
+            () -> new DriftwoodModel(BlockBehaviour.Properties.of().sound(SoundType.WOOD).dynamicShape().noOcclusion())
+    );
+
+    public static final Supplier<Block> BOULDER = BLOCKS.register("boulder",
+            () -> new BoulderModel(BlockBehaviour.Properties.of().sound(SoundType.STONE).dynamicShape().noOcclusion())
+    );
 
     public static final Supplier<Block> SWITCHGRASS = BLOCKS.register("switchgrass",
-            () -> new DoublePlantBlock(BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).replaceable().noCollission().instabreak().sound(SoundType.GRASS).offsetType(BlockBehaviour.OffsetType.XYZ).ignitedByLava().pushReaction(PushReaction.DESTROY)
-            )
+            () -> new DoublePlantBlock(BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).replaceable().noCollission().instabreak().sound(SoundType.GRASS).offsetType(BlockBehaviour.OffsetType.XYZ).ignitedByLava().pushReaction(PushReaction.DESTROY))
     );
 }
